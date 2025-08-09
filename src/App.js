1486b405bcaf3e58676c1294392d78469e6ab7df
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-09 20:16:08 +0330

    create weather app project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+     <h1 className=' text-amber-600 text-6xl'>kaveh</h1>
+     <h1 className=' text-blue-500 text-6xl'>khorshidi</h1>
+    </div>
+  );
+}
+
+
