2ba3f2f3bd98e4f801ea4106dfc82cbcabb2be63
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-10 19:43:30 +0330

    edit components menu and weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-// import Menu from "./components/Menu-section/Menu.js"
-import Menu from "./components/Menu-section/Menu.js"
-import ShowWeather from "./components/Weather-section/ShowWeather.js"
-
-
 export default function App() {
    return (
       <div>
-         <Menu />
-         <ShowWeather />
+
+         <div className="Menu-Section">
+
+         </div>
+
+         <div className="Weather-Section">
+
+         </div>
+         
       </div>
    )
 }
--- src/components/Weather-section/ShowWeather.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ShowWeather() {
-  return (
-    <div>
-      Weather
-    </div>
-  )
-}
