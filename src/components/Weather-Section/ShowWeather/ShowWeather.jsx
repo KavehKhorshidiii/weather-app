1c62f5bc25c697fe3c71318825a15cea54cdcfbe
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-10 23:44:23 +0330

    set weather image

--- src/components/Weather-Section/ShowWeather/ShowWeather.jsx
+++ src/components/Weather-Section/ShowWeather/ShowWeather.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function ShowWeather() {
   return (
     <div className=' w-7/10 border-l-2 border-white/60'>
-      <div></div>
+      <div className='flex justify-center items-center'>
+        <img src="./show weather img/cloud.png" alt="" />
+      </div>
       <div className=' flex flex-col items-center'>
         <span className=' text-white font-bold text-9xl'>21</span>
         <span className=' text-white text-7xl'>Tommorow</span>
