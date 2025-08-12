6fc228c4bf841f18bd620f7d7282472e2e58e38d
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-12 19:21:02 +0330

    completed app routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,8 +3,7 @@ import Profile from "../components/Menu/profile/profile"
 import Home from "../components/Menu/home/home"
 
 export const Routes = [
-    {path:'/' , element:<Home></Home>},
-    {path:'/links' , element:<Links></Links>},
-    {path:'/profile' , element:<Profile></Profile>},
+    { path: "/", element: <Home></Home> },
+    { path: "/links", element: <Links></Links> },
+    { path: "/profile", element: <Profile></Profile> },
 ]
-
