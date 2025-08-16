c60b264db21d191c608bb4f3bdf8bd5542846059
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-16 18:56:19 +0330

    fix dark mode bug

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import MyContextProvider from "./myContext/myContextProvider"
 export default function App() {
     return (
         <MyContextProvider>
-            <div className=" md:flex md:flex-row-reverse h-screen bg-white dark:bg-black">
+            <div className=" md:flex md:flex-row-reverse h-screen  bg-black ">
 
                 <div className="md:w-7/12">
                     <WeatherSection />
                 </div>
 
-                <div className="md:w-5/12 dark:bg-white bg-black px-5 md:px-12 pt-2 md:overflow-auto ">
+                <div className="md:w-5/12 dark:bg-black bg-white px-5 md:px-12 pt-2 md:overflow-auto ">
                     <div className=" sticky top-0 py-3 ">
                         <Menu></Menu>
                     </div>
