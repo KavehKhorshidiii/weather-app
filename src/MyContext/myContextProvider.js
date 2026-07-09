e562c3d1e8242755baab7c5811df0d36a1541f63
Kaveh <kavehkhorshidiii@gmail.com>
2026-07-09 20:45:42 +0330

    commit

--- src/MyContext/myContextProvider.js
+++ src/MyContext/myContextProvider.js
@@ -3,22 +3,22 @@ import { createContext, useState } from "react"
 export const MyContext = createContext()
 
 export default function MyContextProvider({ children }) {
-    // City coordinates
-    const [coords, setCoords] = useState(null)
-    // Confirmed city and country name
-    const [confirmedCity, setConfirmedCity] = useState(null)
-    // Show Error Alert
-    const [error, setError] = useState(false)
+   // City coordinates
+   const [coords, setCoords] = useState(null)
+   // Confirmed city and country name
+   const [confirmedCity, setConfirmedCity] = useState(null)
+   // Show Error Alert
+   const [error, setError] = useState(false)
 
-    return <MyContext.Provider
-     value={{
-        error,
-        setError,
-        coords, 
-        setCoords, 
-        confirmedCity, 
-        setConfirmedCity 
-    }}>
-        {children}
-    </MyContext.Provider>
+   return <MyContext.Provider
+      value={{
+         error,
+         setError,
+         coords,
+         setCoords,
+         confirmedCity,
+         setConfirmedCity
+      }}>
+      {children}
+   </MyContext.Provider>
 }
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,7 +3,7 @@ import History from "../components/MainSection/History/History"
 import Home from "../components/MainSection/Home/Home"
 
 export const Routes = [
-    { path: "/", element: <Home/> },    //Home page
-    { path: "/links", element: <Links/> },    //Link page
-    { path: "/profile", element: <History/> },    //History page
+   { path: "/", element: <Home /> },    //Home page
+   { path: "/links", element: <Links /> },    //Link page
+   { path: "/profile", element: <History /> },    //History page
 ]
